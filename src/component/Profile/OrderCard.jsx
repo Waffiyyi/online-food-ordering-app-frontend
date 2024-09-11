60a27502d84cd1072fe3841d2a6208ef2e9a8c2d
Waffiyyi <fasholawafiyyi@gmail.com>
2024-09-11 08:13:23 +0100

    updated vite config to fix 404 not found error

--- src/component/Profile/OrderCard.jsx
+++ src/component/Profile/OrderCard.jsx
@@ -9,7 +9,7 @@ const OrderCard = ({item, order}) => {
                      src={item.food.images[0]} alt={'food image'}/>
                 <div>
                     <p>{item.food.name}</p>
-                    <p>₦{item.food.totalAmount}</p>
+                    <p>₦{order.totalAmount}</p>
                 </div>
             </div>
             <div>
