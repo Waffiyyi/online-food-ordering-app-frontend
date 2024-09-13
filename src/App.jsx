e17bd27ade9e1bb4b6e0022aa04c5490bfe51c63
Waffiyyi <fasholawafiyyi@gmail.com>
2024-09-13 02:19:34 +0100

    handled logout state

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [auth.jwt, dispatch, jwt]);
 
   useEffect(() => {
-    if(auth.user?.role==="ROLE_RESTAURANT_OWNER" && restaurant.usersRestaurant)
+    if(auth.user?.role==="ROLE_RESTAURANT_OWNER" && !restaurant.usersRestaurant)
       dispatch(getRestaurantByUserId(auth.jwt || jwt));
   }, [auth.jwt, auth.user, dispatch, jwt, restaurant.usersRestaurant]);
   return (
