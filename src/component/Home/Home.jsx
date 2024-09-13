ba094787cbf53c61dea5b71c98a4ba500703ee9c
Waffiyyi <fasholawafiyyi@gmail.com>
2024-09-13 13:02:44 +0100

    uodated home component

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -5,46 +5,57 @@ import RestaurantCard from "../Restaurant/RestaurantCard.jsx";
 import {useDispatch, useSelector} from "react-redux";
 import {getAllRestaurantAction} from "../../State/Restaurant/Action.js";
 import {useNavigate} from "react-router-dom";
+
 const Home = () => {
-    const dispatch = useDispatch()
-    const jwt = localStorage.getItem("jwt")
-    const {restaurant, auth} = useSelector(store=> store)
+  const dispatch = useDispatch()
+  const jwt = localStorage.getItem("jwt")
+  const {restaurant, auth} = useSelector(store => store)
 
-    useEffect(()=>{
-        if(auth.jwt || jwt)
-            dispatch(getAllRestaurantAction(jwt))
-    }, [auth.jwt, dispatch, jwt])
+  useEffect(() => {
+    if (auth.jwt || jwt)
+      dispatch(getAllRestaurantAction(jwt))
+  }, [auth.jwt, dispatch, jwt])
 
 
-    return (
-        <div className={'pb-10'}>
-            <section className={'banner -z-50 relative flex flex-col justify-center items-center'}>
-                <div className={'w-[50vw] z-10 text-center'}>
-                    <p className={'text-2xl lg:text-6xl font-bold z-10 py-5'}>CraveCourier</p>
-                    <p className={'z10 text-gray-300 text-xl lg:text-4xl'}>Taste the Convenience: Food, Fast and Delivered</p>
-                </div>
-                <div className={'cover absolute top-0 left-0 right-0'}>
+  return (
+    <div className={'pb-10'}>
+      <section className={'banner -z-50 relative flex flex-col justify-center items-center'}>
+        <div className={'w-[50vw] z-10 text-center'}>
+          <p className={'text-2xl lg:text-6xl font-bold z-10 py-5'}>CraveCourier</p >
+          <p className={'z10 text-gray-300 text-xl lg:text-4xl'}>Taste the
+                                                                 Convenience:
+                                                                 Food, Fast and
+                                                                 Delivered</p >
+        </div >
+        <div className={'cover absolute top-0 left-0 right-0'}>
 
-                </div>
-                <div className={'fadeout'}>
+        </div >
+        <div className={'fadeout'}>
 
-                </div>
-            </section>
-            <section className={'p-10 lg:py-10 lg:px-20'}>
-                <p className={'text-2xl font-semibold text-gray-400 py-3 pb-10'}>Top Meals</p>
-                <MultiItemCarousel/>
-            </section>
-            <section className={'px-5 lg:px-20 pt-10'}>
-                <h1 className={'text-2xl font-semibold text-gray-400 pb-8'}> Login To Order From Top Tier Restaurants </h1>
-                <div className={'flex flex-wrap items-center justify-around gap-5'}>
-                    {
-                        restaurant.restaurants.map((item)=><RestaurantCard key={item.id} item={item}/>)
-                    }
+        </div >
+      </section >
+      <section className={'p-10 lg:py-10 lg:px-20'}>
+        <p className={'text-2xl font-semibold text-gray-400 py-3 pb-10'}>Top
+                                                                         Meals</p >
+        <MultiItemCarousel />
+      </section >
+      <section className={'px-5 lg:px-20 pt-10'}>
+        <h1 className={'text-2xl font-semibold text-gray-400 pb-8'}>
+          {!auth.user ?
+            "Login To Order From Top Tier Restaurants" :
+            "Order From Our Handpicked Favorites"}</h1 >
+        <div className={'flex flex-wrap items-center justify-around gap-5'}>
+          {
+            restaurant.restaurants.map((item) => <RestaurantCard
+              key={item.id}
+              item={item}
+            />)
+          }
 
-                </div>
-            </section>
-        </div>
-    );
+        </div >
+      </section >
+    </div >
+  );
 };
 
 export default Home;
